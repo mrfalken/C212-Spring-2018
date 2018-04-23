db84933323b384921bc5e4de280adbfa55ff4720
jimconwa <jimconwa@iu.edu>
2018-04-23 16:59:30 -0400

    Added files via uplaod

--- Project/src/Login.java
+++ Project/src/Login.java
@@ -1,7 +1,32 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class Login {
-	public static void main(String[] args)
+public class Login 
+{
+	private String username;
+	private String password;
+	private Scanner sc;
+	PrintWriter output;
+	
+	public Login(String username, String password)
 	{
-		System.out.println("Hey");
+		
+	}
+	
+	public void addUser(String username, String password)
+	{
+		sc = new Scanner(System.in);
+		System.out.println("Create a username: ");
+		username = sc.nextLine();
+		System.out.println("Create a password: ");
+		password = sc.nextLine();
+		
+		try {
+			output = new PrintWriter("Login.txt");
+			output.println(username+", "+password);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
