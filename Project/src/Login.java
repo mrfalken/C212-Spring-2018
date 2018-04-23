0f1cd381b5986ed18e2ec2c6cb49497b0a2fbd73
jimconwa <jimconwa@iu.edu>
2018-04-23 18:43:53 -0400

    Push to new branch

--- Project/src/Login.java
+++ Project/src/Login.java
@@ -1,32 +1,44 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Login 
 {
-	private String username;
-	private String password;
-	private Scanner sc;
-	PrintWriter output;
+	private static Scanner inputReader;
+	private static Scanner fileReader;
+	private static PrintWriter output;
 	
-	public Login(String username, String password)
+	public static void addUser(String username, String password)
 	{
-		
-	}
-	
-	public void addUser(String username, String password)
-	{
-		sc = new Scanner(System.in);
+		inputReader = new Scanner(System.in);
 		System.out.println("Create a username: ");
-		username = sc.nextLine();
+		username = inputReader.nextLine();
 		System.out.println("Create a password: ");
-		password = sc.nextLine();
+		password = inputReader.nextLine();
 		
 		try {
 			output = new PrintWriter("Login.txt");
 			output.println(username+", "+password);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public static void login(String username, String password)
+	{
+		try {
+			fileReader = new Scanner(new File("login.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		HashMap<String, String> logins = new HashMap<String, String>();
+		fileReader.useDelimiter(", ");
+		while(fileReader.hasNext())
+		{
+			
 		}
 	}
 }
