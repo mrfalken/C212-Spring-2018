a07350d2348883ab31e4ba1952b74af10577d6a4
jimconwa <jimconwa@iu.edu>
2018-04-25 16:05:46 -0400

    Did some of payment

--- Project/src/Payment.java
+++ Project/src/Payment.java
@@ -1,4 +1,48 @@
 
-public class Payment {
-
+public class Payment 
+{
+	private double tax;
+	private double gratuity;
+	private double subtotal;
+	private double total;
+	private String paymentType;
+	private final double TAX_PERCENT = 0.08;
+	
+	public Payment(Order finalOrder, double tip)
+	{
+		calculateSubtotal(finalOrder);
+		calculateTax();
+		calculateGratuity(tip);
+		calculateTotal();
+	}
+	
+	public void calculateSubtotal(Order finalOrder)
+	{
+		subtotal = 0;//finalOrder.getPrice();
+	}
+	
+	public void calculateTax()
+	{
+		 tax = subtotal*TAX_PERCENT;
+	}
+	
+	public void calculateGratuity(double tip)
+	{
+		gratuity = tip;
+	}
+	
+	public void calculateTotal()
+	{
+		total += subtotal+tax+gratuity;
+	}
+	
+	public void getPaymentType(String type)
+	{
+		paymentType = type;
+	}
+	
+	public String toString()
+	{
+		return "Subtotal: "+subtotal+"\nTax: "+tax+"\nGratuity: "+gratuity+"\nTotal: "+total;
+	}
 }
