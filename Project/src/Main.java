92f3c21207f49415d28966d48946e11d6c8e5cd6
Garrett Shuman <gshuman@iu.edu>
2018-04-25 17:04:00 -0400

    C:/Program Files/Git/tables and main

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,4 +1,78 @@
+import java.util.Scanner;
 
-public class Main {
+public class Main 
+{
+	
+	
+	public static void main (String [] args)
+	{
+		boolean run = true;
+		Scanner keyboard = new Scanner(System.in);
+		Login log = new Login();
+		if (log.login())
+		{
+		while (run == true)
+		{	
+			System.out.println("What would you like to do today?");
+			System.out.println("1. Place Order");
+			System.out.println("2. Process Payment");
+			System.out.println("3. Change Order");
+			System.out.println("4. Seat Table");
+			System.out.println("5. Clear Table");
+			System.out.println("6. Logout");
+			int o = keyboard.nextInt();
+			
+			if (o == 1)
+			{
+				
+			}
+			else if (o == 2)
+			{
+				
+			}
+			else if (o == 3)
+			{
+				
+			}
+			else if (o == 4)
+			{
+				System.out.println("Please enter the table number you would like to seat");
+				int tn = keyboard.nextInt();
+				System.out.println("Please enter the number of people you would like to seat");
+				int np = keyboard.nextInt();
+				
+				Tables  t = new Tables(tn,np);
+			
+				Tables.printChart();
+			}
+			
+			else if (o == 5)
+			{
+				System.out.println("Please enter the table number you would like to clear");
+				int tn = keyboard.nextInt();
+				
+				Tables.clearTable(tn);
+			
+				Tables.printChart();
+			}
+			
+			else if (o == 6)
+			{
+				System.out.println("Logged Out");
+				run = false;
+			}
+			else
+			{
+				System.out.println("Invalid Input. Please choose an option 1-5");
+			}
+			
+	
+			}
+		}
+		else 
+		{
+			System.out.println("ACCESS DENIED");
+		}
+	}
 
 }
--- Project/src/Tables.java
+++ Project/src/Tables.java
@@ -1,4 +1,63 @@
+import java.util.ArrayList;
 
-public class Tables {
+public class Tables 
+{
+	private int tableNum;
+	private int seatsFilled;
+	private static ArrayList <Tables> seatingChart = new ArrayList<Tables>();
+	public Tables(int num, int seatsF)
+	{
+		
+		setTableNum(num);
+		setSeatsFilled(seatsF);
+		seatingChart.add(this);
+	}
+	
+	
+	public int getSeatsFilled() {
+		return seatsFilled;
+	}
+	public void setSeatsFilled(int seatsFilled) {
+		this.seatsFilled = seatsFilled;
+	}
+	public int getTableNum() {
+		return tableNum;
+	}
+	public void setTableNum(int tableNum) {
+		this.tableNum = tableNum;
+	}
+	
+	public boolean isOccupied()
+	{
+		if(this.getSeatsFilled() == 0)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public static void printChart()
+	{
+		for (int i = 0; i < seatingChart.size(); i ++)
+		{
+			System.out.print(seatingChart.get(i));
+		}
+	}
+	
+	public String toString()
+	{
+		return "Table Num: " + tableNum + " Seats Filled: " + seatsFilled;
+	}
+	
+	public static void clearTable(int n)
+	{
+		for (int i = 0; i < seatingChart.size(); i ++)
+		{
+			if (seatingChart.get(i).tableNum == n)
+			{
+				seatingChart.remove(i);
+			}
+		}
+	}
 
 }
