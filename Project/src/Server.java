9c3b404c0d1cee36a74e5a7019a504a36572c9e8
mggilchr <mggilchr@iu.edu>
2018-04-25 17:25:45 -0400

    Add files via upload
    Commit

--- Project/src/Server.java
+++ Project/src/Server.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Server {
-
+public class Server 
+{
+	public Server()
+	{ }
+	
+	public Server(String user, String pass)
+	{
+		user = "server";
+		pass = "server";
+		Login.login();
+	}
+	
+	public String takeOrder(String a, String b)
+	{
+		String order = a + ", " + b;
+		return order;
+	}
+	
+	public void viewPreviousTables()
+	{
+		
+	}
+	
+	
 }
