52b8f47e77eb30c5f7df29a3547a43fab77afe09
Mitch Gilchrist <mggilchr@iu.edu>
2018-04-25 17:06:06 -0400

    start of server and manager

--- Project/src/Manager.java
+++ Project/src/Manager.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Manager {
-
+public class Manager 
+{
+	private String order;
+	
+	public Manager()
+	{ }
+	
+	public Manager(String user, String pass)
+	{
+		user = "manager";
+		pass = "manager";
+		Login.login();
+	}
+	
+	public void changeServerPassword(int n)
+	{
+		
+	}
+	
+	public void addServer(ArrayList s)
+	{
+		
+	}
+	
+	 public void deleteServer(ArrayList s)
+	 {
+		 
+	 }
+	
+	 public double applyDiscount(int n)
+	 {
+		 int total = n;
+		 total = total / 10;
+		 total = n - total;
+		 return total;
+	 }
+	
+	public void changeOrder(Order o)
+	{
+		
+	}
 }
